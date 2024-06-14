376747518d4198a66317fbd6298fd0f9f2659317
jihanizzathul <105841104021@student.unismuh.ac.id>
2024-06-14 11:56:05 +0800

    box view-jihan

--- App.js
+++ App.js
@@ -1,49 +1,38 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const App = () => {
-  return (
-    <View style ={{
-      flex : 1,
-    }}>
-
-    <View style = {{
-      flex :1,
-      flexDirection : 'row'
-    }}>
-
-      <View style = {{
-        flex :1,
-        backgroundColor : 'pink',
+  return ( 
+    <View style={{flex: 1}}>
+      <View style={{
+        flex: 1, 
+        justifyContent: 'flex-start', 
+        alignItems: 'flex-start'
       }}>
-
-    </View>
-    <View style = {{
-      flex :1,
-      backgroundColor : 'purple',
-    }}>
-
-    </View>
-    </View>
-
-    <View style = {{
-        flex :1,
-        backgroundColor : 'black',
+        <View style={{
+          width: 100, 
+          height: 100, 
+          backgroundColor: 'red'
+        }}>
+        </View>
+      </View>
+    
+      <View style={{
+        flex: 1, 
+        justifyContent: 'flex-end', 
+        alignItems: 'flex-end'
       }}>
-
-  </View>
-
-    <View style = {{
-        flex :1,
-        backgroundColor : 'grey',
-      }}>
-
-  </View>
-  </View>
-
+        <View style={{
+          width: 100, 
+          height: 100, 
+          backgroundColor: 'blue'
+        }}>
+        </View>
+      </View>
+    </View>
   )
 }
 
 export default App
-  
+
+const styles = StyleSheet.create({})
