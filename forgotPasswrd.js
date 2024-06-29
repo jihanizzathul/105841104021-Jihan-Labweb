f490ff432d9c0f2ec6cf5ea05f322fb969d98726
jihanizzathul <105841104021@student.unismuh.ac.id>
2024-06-29 22:25:27 +0800

    forgot password

--- forgotPasswrd.js
+++ forgotPasswrd.js
@@ -1,4 +1,4 @@
-import {Text, View, TextInput,TouchableOpacity, } from 'react-native'
+import {Text, View, TextInput,TouchableOpacity } from 'react-native'
 import React from 'react';
 import { useFonts } from 'expo-font';
 
