127055c98582cd957746ec0b934afce3375e0d03
jihanizzathul <105841104021@student.unismuh.ac.id>
2024-06-29 23:04:27 +0800

    forgotpassword

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {Text, View, TextInput,TouchableOpacity,Image } from 'react-native'
+import {Text, View, TextInput,TouchableOpacity } from 'react-native'
 import React from 'react';
 import { useFonts } from 'expo-font';
 
@@ -45,7 +45,7 @@ const TextInputCustom = ({ placeholder, color, typeKeyboard })=> {
     );
 }
 
-const Login= () => {
+const ForgotPassword= () => {
   const [dapatFont] = useFonts({
     'MetroBlack': require('./assets/fonts/Metropolis-Black.otf'),
     'MetroBold' : require ('./assets/fonts/Metropolis-Bold.otf'),
@@ -68,96 +68,32 @@ const Login= () => {
     }}>
 
     <Text style={{
-        fontSize: 50,
+        fontSize: 40,
         fontFamily: 'MetroBold',
         marginTop: 20,
-        marginBottom:50,
-      }}>Login</Text>
+        marginBottom:80,
+      }}>Forgot Password</Text>
 
-    <View style={{
+      <View style={{
         alignItems: 'center',
-        width:'100%',
+        width: '100%'
       }}>
 
-      <TextInputCustom placeholder="Name" color="black" typeKeyboard="default"/>
-      <TextInputCustom placeholder="Password" color="black" typeKeyboard="default" />
-
-      <TouchableOpacity style ={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          width: 320,
-          marginBottom:20,
-          }}>
-
-        <Text style={{
+    <Text style={{
           fontFamily: 'MetroMedium',
           marginRight:5,
-          fontSize:14,
-          color: 'red',
-          }}> Forgot Password?</Text>
-
-        <Image source={require('./assets/arrw.png')} style={{
-            width: 20,
-            height:20,
-          }}/>
-        </TouchableOpacity> 
-      
-      <View style={{marginBottom:100}}>
-        <ButtonCustom text="LOGIN" color="red"/>
-        </View>
-
-        <Text style={{
-          fontFamily: 'MetroMedium',
-          fontSize: 14,
-          color : '#555',
-          marginBottom: 10,
-          marginTop:10,
-          textAlign: 'center',
-          textDecorationLine: 'underline',
-          width :320,
-        }}> Or login with social account</Text>
-
-        <View style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom:30,
-            width:320,
-        }}>
-
-        <TouchableOpacity style={{
-              width: 50,
-              height: 50,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginHorizontal: 8,
-              }}>
-
-          <Image source={require('./assets/google.png')} style={{
-              width: 40,
-              height: 40,
-              }}/>
-          </TouchableOpacity>
-                
-            <TouchableOpacity style={{
-              width: 50,
-              height: 50,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginHorizontal: 8,
-                }}>
-              
-            <Image source={require('./assets/fb.png')} style={{
-                width: 40,
-                height: 40,
-              }}/>
-                </TouchableOpacity>
-
-            </View>
-        </View>
+          fontSize:12,
+          textAlign : 'center',
+          }}> Please, enter your email addres. You will receive a link to create a new password via email</Text>
+      <View style={{marginBottom:50}}>
+      <TextInputCustom placeholder="enter your email" color="black" typeKeyboard="email-address"/>
       </View>
+
+        <ButtonCustom text="Confirm mail" color="red"/>
+      </View>
+    </View>
+    
   );
 }
 
-export default Login
+export default ForgotPassword
