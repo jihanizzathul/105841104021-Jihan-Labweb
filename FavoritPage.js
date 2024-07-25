734c7e666e7526afed19d9fcfb396b09d40380e7
jihanizzathul <105841104021@student.unismuh.ac.id>
2024-07-25 18:04:33 +0800

    navigation dan taskbar

---
+++ FavoritPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FavoritPage = () => {
+  return (
+    <View>
+      <Text>FavoritPage</Text>
+    </View>
+  )
+}
+
+export default FavoritPage
+
+const styles = StyleSheet.create({})
